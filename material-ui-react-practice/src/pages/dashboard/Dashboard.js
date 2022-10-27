9554e582bdb49c7d3a3bbaeec80b3bd8218e0a9d
dbw7 <90856056+dbw7@users.noreply.github.com>
2022-10-26 23:09:45 -0400

    use this

--- material-ui-react-practice/src/pages/dashboard/Dashboard.js
+++ material-ui-react-practice/src/pages/dashboard/Dashboard.js
@@ -3,7 +3,24 @@ import './Dashboard.css';
 const Dashboard = () => {
     return(
         <div className='dashboard-main'>
+            <div className='data'>
+                <table className="table">
+                    <thead className="sticky-top" id="proxy-identifiers">
+                        <tr className="">
+                            <th scope="col">#</th>
+                            <th scope="col">IP</th>
+                            <th scope="col">PORT</th>
+                            <th scope="col">USER</th>
+                            <th scope="col">PASS</th>
+                            <th scope="col">SPEED (MS)</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody id="table-body">
             
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
